fb580bb6c4142a164f888380d76a3b8bec962112
sasikalarajendran <sasikalarvsb@gmail.com>
2017-06-19 15:35:44 +0530

    positive or negative
    c program

---
+++ posorneg.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n)
+if(n<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is positive");
+}
+
+return 0;
+}
