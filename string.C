61b02ee0b10514eb0d76854234fd02e6595f1a06
sasikalarajendran <sasikalarvsb@gmail.com>
2017-06-21 16:28:47 +0530

    reverse a string
    c programme

---
+++ string.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+char str;
+printf("enter a original string");
+scanf("%s",&str);
+for(i=str;i<=str;str--)
+{
+j=strlen-1;
+j--;
+}
+printf("the reversed string is");
+return 0;
+}
