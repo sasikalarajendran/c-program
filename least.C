7944099d922190ab6126f96a491a29a9b47189d6
sasikalarajendran <sasikalarvsb@gmail.com>
2017-06-28 15:07:07 +0530

    delete least number
    c programme

---
+++ least.C
@@ -0,0 +1,27 @@
+#include<iostream.h>
+using namespace std;
+void buildLowestnumberRec(string str,int n,string &res)
+{
+if(n==0)
+res.append(str);
+return;
+}
+int len=str.length();
+if(len<=n);
+return;
+int minIndex=0;
+for(int i=1;i<n;i++)
+if(str[i]<str[minIndex])
+minIndex=i;
+res.push_back(str[minIndex]);
+string new_str=str.substr(minIndex+1,len-minIndex);
+buildLowestNumber(str,n,res);
+return res;
+}
+int main()
+{
+string str="121198";
+int n=2;
+cout<<buildLowestNumber(str,n);
+return 0;
+}
