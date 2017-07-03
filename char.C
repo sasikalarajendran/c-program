4211b70a6618bb991e427a89b439480434cf0ba1
sasikalarajendran <sasikalarvsb@gmail.com>
2017-07-03 16:26:23 +0530

    count the numerical character
    c programme

---
+++ char.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter any character in string");
+scanf("%c",&ch);
+if(ch>='0'&&ch<='9')
+{
+printf("the numerical character is");
+}
+return 0;
+}
